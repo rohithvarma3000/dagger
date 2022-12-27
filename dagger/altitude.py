3087c8f74058ecbb16029b56524273a54e9c1931
Pratham <prathamks.email@gmail.com>
2022-12-27 12:48:51 +0530

    restructure decoding method

--- dagger/altitude.py
+++ dagger/altitude.py
@@ -1,18 +1,20 @@
+import struct
 from dagger.utils import get_direction_out_bytes, get_header_bytes, calculate_crc
 
+class AltitudeData:
+    def __init__(self,altitude,vario):
+        self.altitude=altitude
+        self.vario=vario
 
-class SetAltitude:
-    
+
+class Altitude:
     __msg_code = 109 
     __msg_length = 6
 
     def __init__(self, connection):
         self._connection = connection
-        self.altitude=0
-        self.vario=0
 
-
-    def _request(self): #function for requesting the out packet
+    def get(self): #function for requesting the out packet
         header  = get_header_bytes()
         direction = get_direction_in_bytes() #Requesting the out packet using an in direction as mentioned in docs
         length_bytes = bytearray(SetAltitude.__msg_length.to_bytes(1, byteorder="little"))
@@ -24,16 +26,16 @@ class SetAltitude:
         self._connection.send(packet)
 
         try:
-            self._response()
-            print(self.altitude,self.vario)
+            header_out=struct.unpack('cc',self._connection.recv(2))[0]
+            direction_out=struct.unpack('c',self._connection.recv(1))[0]
+            length_out=struct.unpack('B',self._connection.recv(1))[0]
+            code_out=struct.unpack('B',self._connection.recv(1))[0]
+            altitude=struct.unpack('<i',self._connection.recv(4))[0] #signed int of 4 bytes hence i
+            vario=struct.unpack('<h',self._connection.recv(2))[0] #signed int of 2 bytes hence h
+            crc_out=struct.unpack('B',self._connection.recv(1))[0]
+
+            obj=AltitudeData(altitude, vario)
+            return obj
+
         except:
             print("Data not recieved/some error occured")
-    
-    def _response(self): #function for parsing the response
-        header=self._connection.recv(2).decode('utf-8')
-        direction=self._connection.recv(1).decode('utf-8')
-        length=self._connection.recv(1).decode('utf-8')
-        code=self._connection.recv(1).decode('utf-8')
-        self.altitude=int(self._connection.recv(4).decode('utf-8'))
-        self.vario=int(self._connection.recv(2).decode('utf-8'))
-        crc=self._connection.recv(1).decode('utf-8')
