63811cc3297ca55d114bd6553a76004b9bf57f58
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2022-12-31 19:24:50 +0530

    Add calibration commands

---
+++ dagger/mag_calibration.py
@@ -0,0 +1,26 @@
+from dagger.utils import get_header_bytes, get_direction_in_bytes, calculate_crc
+
+
+class MagCalibartion:
+    __msg_length = 0
+    __msg_code = 206
+
+    def __init__(self, connection):
+        self._connection = connection
+        self.cmd = 0
+
+    def acc_calibartion(self):
+        header = get_header_bytes()
+        direction = get_direction_in_bytes()
+        length_bytes = bytearray(
+            MagCalibartion.__msg_length.to_bytes(1, byteorder="little")
+        )
+        code_bytes = bytearray(
+            MagCalibartion.__msg_code.to_bytes(1, byteorder="little")
+        )
+
+        message = length_bytes + code_bytes
+        crc = calculate_crc(message)
+        packet = header + direction + message
+        packet.append(crc)
+        self._connection.send(packet)
