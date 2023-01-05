2f0fc752be25df0a2a25bcb7f4db91c13899a05d
Nagesh Bansal <nageshbansal59@gmail.com>
2023-01-06 01:23:25 +0530

    Fixes Alitude OUT package
    Signed-off-by: Nagesh Bansal <nageshbansal59@gmail.com>

--- dagger/__init__.py
+++ dagger/__init__.py
@@ -1,7 +1,7 @@
 from dagger.connect import PlutoConnection
 from dagger.set_command import SetCommand, CmdType
 from dagger.set_raw_rc import SetRawRC
-from dagger.altitude import Altitude
+from dagger.altitude import SetAltitude
 from dagger.attitude import SetAttitude
 from dagger.acc_calibration import AccCalibration
 from dagger.mag_calibration import MagCalibration
--- dagger/altitude.py
+++ dagger/altitude.py
@@ -1,48 +1,74 @@
 import struct
+import time
 from dagger.utils import get_direction_in_bytes, get_header_bytes, calculate_crc, ZERO
 
 
-class AltitudeData:
-    def __init__(self, altitude, vario):
-        self.altitude = altitude
-        self.vario = vario
-
-
-class Altitude:
+class SetAltitude:
+    
     __msg_code = 109
     __msg_length = 6
 
     def __init__(self, connection):
         self._connection = connection
+        self.altitude = {}
 
-    def get_(self):  # function for requesting the out packet
-        header = get_header_bytes()
-        direction = (
-            get_direction_in_bytes()
-        )  # Requesting the out packet using an in direction as mentioned in docs
+    def altitude_request(self):
+        """Requests the OUT package."""
+        header  = get_header_bytes()
+        direction = get_direction_in_bytes()
         length_bytes = bytearray(ZERO.to_bytes(1, byteorder="little"))
-        code_bytes = bytearray(Altitude.__msg_code.to_bytes(1, byteorder="little"))
-        message = length_bytes + code_bytes  # no payload data
+        code_bytes = bytearray(SetAltitude.__msg_code.to_bytes(1, byteorder="little"))
+        
+        message =  length_bytes + code_bytes
         crc = calculate_crc(message)
-        packet = header + direction + message
+        packet =  header + direction + message
         packet.append(crc)
         self._connection.send(packet)
 
         try:
-            header_out = struct.unpack("cc", self._connection.recv(2))[0]
-            direction_out = struct.unpack("c", self._connection.recv(1))[0]
-            length_out = struct.unpack("B", self._connection.recv(1))[0]
-            code_out = struct.unpack("B", self._connection.recv(1))[0]
-            altitude = struct.unpack("<i", self._connection.recv(4))[
-                0
-            ]  # signed int of 4 bytes hence i
-            vario = struct.unpack("<h", self._connection.recv(2))[
-                0
-            ]  # signed int of 2 bytes hence h
-            crc_out = struct.unpack("B", self._connection.recv(1))[0]
-
-            obj = AltitudeData(altitude, vario)
-            return obj
-
+            start = time.time()
+            status = self.altitude_response(start)
+            print(self.altitude)
         except:
-            print("Data not recieved/some error occured")
+            print("Data not recieved.")
+
+
+    def altitude_response(self, start):
+        """Recieves the OUT packages."""
+        while True:
+            header = struct.unpack('c',self._connection.recv(1))[0]
+            if header.decode('utf-8') == '$':
+                print(header.decode('utf-8'))
+                header_m = struct.unpack('c', self._connection.recv(1))[0]
+                print(header_m)
+                if header_m.decode('utf-8') == 'M':
+                    direction = struct.unpack('c', self._connection.recv(1))[0]
+                    print(direction)
+                    if direction.decode('utf-8') == '>':
+                        size = struct.unpack('B', self._connection.recv(1))[0]
+                        code = struct.unpack('B', self._connection.recv(1))[0]
+                        print(size, code)
+                        if size == self.__msg_length and code == self.__msg_code:
+                            data = self._connection.recv(6)
+                            crc = self._connection.recv(1)
+                            temp = struct.unpack('<ih',data)
+                            elapsed = time.time() - start
+                            self.altitude['altitude']=float(temp[0])
+                            self.altitude['vatio']=float(temp[1])
+                            self.altitude['elapsed']=round(elapsed,3)
+                            self.altitude['timestamp']="%0.2f" % (time.time(),) 
+                            break
+    
+        return True
+
+
+
+
+
+            # altitude = struct.unpack("<i", self._connection.recv(4))[
+            #     0
+            # ]  # signed int of 4 bytes hence i
+            # vario = struct.unpack("<h", self._connection.recv(2))[
+            #     0
+            # ]  # signed int of 2 bytes hence h
+            # crc_out = struct.unpack("B", self._connection.recv(1))[0]
