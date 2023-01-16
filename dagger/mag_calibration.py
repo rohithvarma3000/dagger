b886d20317c5e16d49bd5f6025f0680ddc024bfa
Nagesh Bansal <nageshbansal59@gmail.com>
2023-01-17 03:00:52 +0530

    Adds the docs for mag_calibration
    Signed-off-by: Nagesh Bansal <nageshbansal59@gmail.com>

--- dagger/mag_calibration.py
+++ dagger/mag_calibration.py
@@ -1,7 +1,22 @@
+"""Calibrates the magnetometer"""
 from dagger.utils import get_header_bytes, get_direction_in_bytes, calculate_crc
 
 
 class MagCalibration:
+    """Calibrates the magnetometer..
+
+    Parameters
+    ----------
+    connection : PlutoConnection
+        Pluto connection object for communicating with ``Pluto``.
+
+    Examples
+    --------
+    >>> t = dagger.PlutoConnection()
+    >>> t.connect(('Pluto_IP', Pluto_port))
+    >>> mag_cal = dagger.MagCalibration(t)
+
+    """
     __msg_length = 0
     __msg_code = 206
 
@@ -10,6 +25,16 @@ class MagCalibration:
         self.cmd = 0
 
     def mag_calibartion(self):
+        """Sends the request to calibrate the magnetometer.
+
+        Examples
+        --------
+        >>> mag_cal.mag_calibration()
+
+        Note
+        ----
+        Rotate the drone across all its axis until the process.
+        """
         header = get_header_bytes()
         direction = get_direction_in_bytes()
         length_bytes = bytearray(
